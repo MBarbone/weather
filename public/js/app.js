53ebd3bf2637935ef14788b4069976abcd071e0b
Mike <michael.barbone@gmail.com>
2019-07-17 15:06:21 -0400

    Changes to styling including navbar and search included in header so available on all routes.

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener("submit", e => {
   e.preventDefault();
   const location = search.value;
 
-  messageOne.textContent = "Loading...";
+  messageOne.textContent = "";
 
   fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
     response.json().then(data => {
