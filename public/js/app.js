549c283472b421089897eb5b315038ba7931d70e
Mike <michael.barbone@gmail.com>
2019-07-16 22:29:10 -0400

    App fully functional

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,22 @@
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.getElementById("messageOne");
+const messageTwo = document.getElementById("messageTwo");
 
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
   const location = search.value;
-  console.log(location);
+
+  messageOne.textContent = "Loading...";
+
   fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
     response.json().then(data => {
       if (data.error) {
-        console.log(data.error);
+        messageOne.textContent = data.error;
+        messageTwo.textContent = "";
       } else {
-        console.log(data.location);
-        console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
       }
     });
   });
