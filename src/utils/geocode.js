2aebf260c2e812ccf1af503adab5a83e486b0b76
Mike <michael.barbone@gmail.com>
2019-07-21 17:46:30 -0400

    Heroku not recognizing env vars in .env file

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,7 +4,7 @@ const request = require("request");
 const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address
-  )}.json?access_token=pk.eyJ1IjoibWJhcmJvbmUxMyIsImEiOiJjanh0aTg0d3gwdXI4M2Judmd6eTVnZW80In0.qqJ8Acb__gdUzdzECNIRUA&limit=1`;
+  )}.json?access_token=${process.env.GEOCODE_TOKEN}&limit=1`;
   request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to geocoding service.");
